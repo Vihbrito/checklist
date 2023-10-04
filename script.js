0a2ddf974a58c0d3f8bded690f587c77ccbce943
Vihbrito <vitoriadbrito@gotmail.com>
2023-10-04 19:20:19 -0300

    first commit

--- script.js
+++ script.js
@@ -1,127 +1,108 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const checkboxes = document.querySelectorAll('.checkbox');
-    const completedCount = document.getElementById('completed-count');
-    const totalCount = document.getElementById('total-count');
-    const completedItems = document.getElementById('completed-items');
-    const checklist = document.getElementById('checklist');
-    const summaryCard = document.getElementById('summary-card');
-    const reloadButton = document.getElementById('reload-button');
-
+document.addEventListener("DOMContentLoaded", function () {
+    const checkboxes = document.querySelectorAll(".checkbox");
+    const completedCount = document.getElementById("completed-count");
+    const totalItemCount = document.getElementById("total-count");
+    const completedItems = document.getElementById("completed-items");
+    const reloadButton = document.getElementById("reload-button");
+    
+    
+    const dataElement = document.getElementById("data");
+    
     checkboxes.forEach(function (checkbox) {
-        checkbox.addEventListener('change', updateSummaryAndMoveItem);
+        checkbox.addEventListener("change", updateSummaryAndMoveItem);
     });
-
-    // Função para recarregar a página
+    
+    // RECARREGAR PÁGINA
     function reloadPage() {
         location.reload();
     }
-
-    reloadButton.addEventListener('click', reloadPage);
-
+    
+    reloadButton.addEventListener("click", reloadPage);
+    
     function updateSummaryAndMoveItem() {
-        const checkedCheckboxes = document.querySelectorAll('.checkbox:checked');
+        const checkedCheckboxes = document.querySelectorAll(".checkbox:checked");
         const checkedItems = Array.from(checkedCheckboxes).map(function (checkbox) {
             return checkbox.parentElement.textContent.trim();
         });
-
-        completedCount.textContent = checkedCheckboxes.length;
-        completedItems.textContent = checkedItems.join(', ');
-
-        checkedCheckboxes.forEach(function (checkbox) {
-            const listItem = checkbox.parentElement;
-            listItem.style.display = 'none'; // Esconde o item da lista
-            checklist.removeChild(listItem); // Remove o item da lista principal
-            summaryCard.appendChild(listItem); // Adiciona o item ao card de resumo
-        });
-
-      
-    }
-
-    function saveCheckedItem(item) {
-        const today = new Date();
-        const dateString = today.toDateString(); // Converte a data atual em uma string
-    
-        // Recupera o histórico de itens marcados do localStorage ou cria um array vazio
-        const history = JSON.parse(localStorage.getItem('history')) || [];
-    
-        // Adiciona a data e o item marcado ao histórico
-        history.push({
-            date: dateString,
-            item: item
-        });
-    
-        // Salva o histórico atualizado no localStorage
-        localStorage.setItem('history', JSON.stringify(history));
-    }
-    
-    function updateSummaryAndMoveItem() {
-        const checkedCheckboxes = document.querySelectorAll('.checkbox:checked');
         
-        checkedCheckboxes.forEach(function (checkbox) {
-            const listItem = checkbox.parentElement;
-            listItem.style.display = 'none'; // Esconde o item da lista
-            checklist.removeChild(listItem); // Remove o item da lista principal
-    
-            // Salva o item marcado no localStorage
-            saveCheckedItem(listItem.textContent.trim());
-        });
-    
-        // Atualiza o resumo e o contador de itens marcados
-        const checkedItems = Array.from(checkedCheckboxes).map(function (checkbox) {
-            return checkbox.parentElement.textContent.trim();
-        });
-    
         completedCount.textContent = checkedCheckboxes.length;
-        completedItems.textContent = checkedItems.join(', ');
+        totalItemCount.textContent = checkboxes.length;
+        completedItems.textContent = checkedItems.join(", ");
+        
+        // ATUALIZAR A DATA
+        const dataAtual = new Date();
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        dataElement.textContent = dataAtual.toLocaleDateString('pt-BR', options);
     }
     
-
     updateSummaryAndMoveItem();
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const checkboxes = document.querySelectorAll('.checkbox');
-    const congratulationsMessage = document.getElementById('congratulations-message');
 
-    checkboxes.forEach(function (checkbox) {
-        checkbox.addEventListener('change', checkAllCheckboxes);
+// BOTÃO DARKMODE
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeButton = document.getElementById("dark-mode-button");
+  const body = document.body;
+
+  darkModeButton.addEventListener("click", function () {
+    body.classList.toggle("dark-mode-active");
+  });
+});
+
+
+// PRINT
+document.getElementById("capturar").addEventListener("click", function () {
+  const elementoParaCapturar = document.getElementById(
+    "elemento-para-capturar"
+  );
+  const container = document.getElementById("captura-de-tela");
+
+  html2canvas(elementoParaCapturar).then(function (canvas) {
+    container.appendChild(canvas);
+
+    // Converter o canvas para um URL de dados
+    const imagemURL = canvas.toDataURL("image/png");
+
+    const downloadLink = document.getElementById("download-link");
+    downloadLink.href = imagemURL;
+    downloadLink.style.display = "block";
+    downloadLink.download = "captura_de_tela.png"; // Nome do arquivo para download
+
+    const compartilharButton = document.createElement("button");
+    compartilharButton.textContent = "Compartilhar";
+    compartilharButton.className = "botao-compartilhar";
+    compartilharButton.addEventListener("click", function () {
+      if (navigator.share) {
+        fetch(imagemURL)
+          .then((response) => response.blob())
+          .then((blob) => {
+            const shareData = {
+              files: [
+                new File([blob], "captura_de_tela.png", { type: "image/png" }),
+              ],
+            };
+            navigator
+              .share(shareData)
+              .then(() => console.log("Imagem compartilhada com sucesso"))
+              .catch((error) => console.error("Erro ao compartilhar:", error));
+          })
+          .catch((error) => console.error("Erro ao buscar a imagem:", error));
+      } else {
+        alert("Seu navegador não suporta a função de compartilhamento.");
+      }
     });
 
-    function checkAllCheckboxes() {
-        const allChecked = [...checkboxes].every(function (checkbox) {
-            return checkbox.checked;
-        });
-
-        if (allChecked) {
-            congratulationsMessage.classList.remove('hidden');
-        } else {
-            congratulationsMessage.classList.add('hidden');
-        }
-    }
+    container.appendChild(compartilharButton);
+  });
 });
 
+const compartilharButton = document.createElement("button");
+compartilharButton.textContent = "Compartilhar Captura";
+compartilharButton.className = "compartilhar-button";
+compartilharButton.addEventListener("click", function () {});
+
+container.appendChild(compartilharButton);
+
+// PRINT
 
 
-// redirecionar para histórico
-document.addEventListener('DOMContentLoaded', function () {
-    const viewHistoryButton = document.getElementById('view-history');
-
-    viewHistoryButton.addEventListener('click', function () {
-        window.location.href = 'historico.html'; // Redireciona para a página de histórico
-    });
-});
-
-document.getElementById('reload-button').addEventListener('click', function () {
-    location.reload();
-});
-
-
-// Botão darkmode
-document.addEventListener('DOMContentLoaded', function () {
-    const darkModeButton = document.getElementById('dark-mode-button');
-    const body = document.body;
-
-    darkModeButton.addEventListener('click', function () {
-        body.classList.toggle('dark-mode-active');
-    });
-});
